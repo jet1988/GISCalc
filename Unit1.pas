139dbb9a0bc9c32f301341eb9e77fa9e9d2e4214
jet.ne@mail.ru <jet1988>
2020-05-12 13:47:46 +0300

    new

---
+++ Unit1.pas
@@ -0,0 +1,268 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Mask, MaskEdEx,
+  Vcl.Buttons, AdvSpin;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    MaskEditEx1: TMaskEditEx;
+    MaskEditEx2: TMaskEditEx;
+    MaskEditEx3: TMaskEditEx;
+    MaskEditEx4: TMaskEditEx;
+    MaskEditEx13: TMaskEditEx;
+    MaskEditEx14: TMaskEditEx;
+    BitBtn2: TBitBtn;
+    Label3: TLabel;
+    Label4: TLabel;
+    MaskEditEx5: TMaskEditEx;
+    MaskEditEx6: TMaskEditEx;
+    Label6: TLabel;
+    MaskEditEx7: TMaskEditEx;
+    MaskEditEx8: TMaskEditEx;
+    MaskEditEx9: TMaskEditEx;
+    MaskEditEx10: TMaskEditEx;
+    MaskEditEx11: TMaskEditEx;
+    MaskEditEx12: TMaskEditEx;
+    BitBtn5: TBitBtn;
+    Memo1: TMemo;
+    Label5: TLabel;
+    Label7: TLabel;
+    AdvSpinEdit1: TAdvSpinEdit;
+    AdvSpinEdit2: TAdvSpinEdit;
+    AdvSpinEdit3: TAdvSpinEdit;
+    AdvSpinEdit4: TAdvSpinEdit;
+    AdvSpinEdit5: TAdvSpinEdit;
+    AdvSpinEdit6: TAdvSpinEdit;
+    AdvSpinEdit7: TAdvSpinEdit;
+    AdvSpinEdit8: TAdvSpinEdit;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    procedure DDD(ddd:Real; p:byte);
+    procedure DDMM(dd:Integer; mmm:Real; p:byte);
+    procedure DDMMSS(dd,mm:Integer; sss:Real; p:byte);
+    procedure BitBtn2Click(Sender: TObject);
+    procedure MaskEditEx8Change(Sender: TObject);
+    procedure MaskEditEx10Change(Sender: TObject);
+    procedure MaskEditEx12Change(Sender: TObject);
+    procedure BitBtn5Click(Sender: TObject);
+    procedure MaskEditEx11Enter(Sender: TObject);
+    procedure MaskEditEx9Enter(Sender: TObject);
+    procedure MaskEditEx7Enter(Sender: TObject);
+    procedure MaskEditEx3Enter(Sender: TObject);
+    procedure MaskEditEx5Enter(Sender: TObject);
+    procedure MaskEditEx4Enter(Sender: TObject);
+    procedure MaskEditEx6Enter(Sender: TObject);
+    procedure MaskEditEx1Enter(Sender: TObject);
+    procedure MaskEditEx2Enter(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+  uses ShellApi, Math;
+
+{$R *.dfm}
+
+procedure TForm1.BitBtn5Click(Sender: TObject);
+var URL: string;
+begin
+URL:='https://www.google.ru/maps/place/'+MaskEditEx13.Text+'+'+MaskEditEx14.Text+'/';
+Memo1.Text:=URL;
+ShellExecute( Handle, 'open', PChar(URL), nil, nil, SW_NORMAL );
+end;
+
+procedure TForm1.DDD(ddd:Real; p:byte);
+var mmm, sss : Real;
+    dd, mm : Integer;
+begin
+  //ddd:=StrToFloat(MaskEditEx1.Text);
+
+  dd:= abs(Trunc(ddd));
+  mmm:=RoundTo(abs(Frac(ddd)*60),-6);
+  mm:= abs(Trunc(mmm));
+  sss:=RoundTo(abs(Frac(mmm)*60),-6);
+
+  case p of
+    1: begin
+    MaskEditEx3.Text:=IntToStr(dd);
+    MaskEditEx5.Text:=FloatToStr(mmm);
+
+    MaskEditEx7.Text:=IntToStr(dd);
+    MaskEditEx9.Text:=IntToStr(mm);
+    MaskEditEx11.Text:=FloatToStr(sss);
+
+    MaskEditEx13.Text:=MaskEditEx7.Text+'°'+MaskEditEx9.Text+'′'+IntToStr(Round(sss))+'′′';
+    if ddd>0 then MaskEditEx13.Text:=MaskEditEx13.Text+'N'
+      else MaskEditEx13.Text:=MaskEditEx13.Text+'S';
+    end;
+    2: begin
+    MaskEditEx4.Text:=IntToStr(dd);
+    MaskEditEx6.Text:=FloatToStr(mmm);
+
+    MaskEditEx8.Text:=IntToStr(dd);
+    MaskEditEx10.Text:=IntToStr(mm);
+    MaskEditEx12.Text:=FloatToStr(sss);
+
+    MaskEditEx14.Text:=MaskEditEx8.Text+'°'+MaskEditEx10.Text+'′'+IntToStr(Round(sss))+'′′';
+    if ddd>0 then MaskEditEx14.Text:=MaskEditEx14.Text+'E'
+      else MaskEditEx14.Text:=MaskEditEx14.Text+'W';
+    end;
+  end;
+
+end;
+
+procedure TForm1.DDMM(dd:Integer; mmm:Real; p:Byte);
+var ddd, sss : Real;
+    mm : Integer;
+begin
+  ddd:=RoundTo(dd+(mmm/60),-6);
+  mm:= abs(Trunc(mmm));
+  sss:=RoundTo(abs(Frac(mmm)*60),-6);
+
+  case p of
+    1: begin
+    MaskEditEx1.Text:=FloatToStr(ddd);
+
+    MaskEditEx7.Text:=IntToStr(dd);
+    MaskEditEx9.Text:=IntToStr(mm);
+    MaskEditEx11.Text:=FloatToStr(sss);
+
+    MaskEditEx13.Text:=MaskEditEx7.Text+'°'+MaskEditEx9.Text+'′'+IntToStr(Round(sss))+'′′';
+    if ddd>0 then MaskEditEx13.Text:=MaskEditEx13.Text+'N'
+      else MaskEditEx13.Text:=MaskEditEx13.Text+'S';
+    end;
+    2: begin
+    MaskEditEx2.Text:=FloatToStr(ddd);
+
+    MaskEditEx8.Text:=IntToStr(dd);
+    MaskEditEx10.Text:=IntToStr(mm);
+    MaskEditEx12.Text:=FloatToStr(sss);
+
+    MaskEditEx14.Text:=MaskEditEx8.Text+'°'+MaskEditEx10.Text+'′'+IntToStr(Round(sss))+'′′';
+    if ddd>0 then MaskEditEx14.Text:=MaskEditEx14.Text+'E'
+      else MaskEditEx14.Text:=MaskEditEx14.Text+'W';
+    end;
+  end;
+end;
+
+procedure TForm1.DDMMSS(dd,mm:Integer; sss:Real; p:byte);
+var ddd, mmm : Real;
+begin
+ddd:=RoundTo(dd+(mm/60)+(sss/3600),-6);
+mmm:=RoundTo(abs(Frac(ddd)*60),-6);
+
+case p of
+    1: begin
+    MaskEditEx1.Text:=FloatToStr(ddd);
+
+    MaskEditEx3.Text:=IntToStr(dd);
+    MaskEditEx5.Text:=FloatToStr(mmm);
+
+    MaskEditEx13.Text:=MaskEditEx7.Text+'°'+MaskEditEx9.Text+'′'+IntToStr(Round(sss))+'′′';
+    if ddd>0 then MaskEditEx13.Text:=MaskEditEx13.Text+'N'
+      else MaskEditEx13.Text:=MaskEditEx13.Text+'S';
+    end;
+    2: begin
+    MaskEditEx2.Text:=FloatToStr(ddd);
+
+    MaskEditEx4.Text:=IntToStr(dd);
+    MaskEditEx6.Text:=FloatToStr(mmm);
+
+    MaskEditEx8.Text:=IntToStr(dd);
+    MaskEditEx10.Text:=IntToStr(mm);
+    MaskEditEx12.Text:=FloatToStr(sss);
+
+    MaskEditEx14.Text:=MaskEditEx8.Text+'°'+MaskEditEx10.Text+'′'+IntToStr(Round(sss))+'′′';
+    if ddd>0 then MaskEditEx14.Text:=MaskEditEx14.Text+'E'
+      else MaskEditEx14.Text:=MaskEditEx14.Text+'W';
+    end;
+  end;
+end;
+
+procedure TForm1.MaskEditEx10Change(Sender: TObject);
+begin
+DDMMSS(StrToInt(MaskEditEx8.Text),StrToInt(MaskEditEx10.Text),StrToFloat(MaskEditEx12.Text),2);
+end;
+
+procedure TForm1.MaskEditEx11Enter(Sender: TObject);
+begin
+DDMMSS(StrToInt(MaskEditEx7.Text),StrToInt(MaskEditEx9.Text),StrToFloat(MaskEditEx11.Text),1);
+end;
+
+procedure TForm1.MaskEditEx12Change(Sender: TObject);
+begin
+DDMMSS(StrToInt(MaskEditEx8.Text),StrToInt(MaskEditEx10.Text),StrToFloat(MaskEditEx12.Text),2);
+end;
+
+procedure TForm1.MaskEditEx1Enter(Sender: TObject);
+begin
+DDD(StrToFloat(MaskEditEx1.Text),1);
+end;
+
+procedure TForm1.MaskEditEx2Enter(Sender: TObject);
+begin
+DDD(StrToFloat(MaskEditEx2.Text),2);
+end;
+
+procedure TForm1.MaskEditEx3Enter(Sender: TObject);
+begin
+DDMM(StrToInt(MaskEditEx3.Text),StrToFloat(MaskEditEx5.Text),1);
+end;
+
+procedure TForm1.MaskEditEx4Enter(Sender: TObject);
+begin
+DDMM(StrToInt(MaskEditEx4.Text),StrToFloat(MaskEditEx6.Text),2);
+end;
+
+procedure TForm1.MaskEditEx5Enter(Sender: TObject);
+begin
+DDMM(StrToInt(MaskEditEx3.Text),StrToFloat(MaskEditEx5.Text),1);
+end;
+
+procedure TForm1.MaskEditEx6Enter(Sender: TObject);
+begin
+DDMM(StrToInt(MaskEditEx4.Text),StrToFloat(MaskEditEx6.Text),2);
+end;
+
+procedure TForm1.MaskEditEx7Enter(Sender: TObject);
+begin
+DDMMSS(StrToInt(MaskEditEx7.Text),StrToInt(MaskEditEx9.Text),StrToFloat(MaskEditEx11.Text),1);
+end;
+
+procedure TForm1.MaskEditEx8Change(Sender: TObject);
+begin
+DDMMSS(StrToInt(MaskEditEx8.Text),StrToInt(MaskEditEx10.Text),StrToFloat(MaskEditEx12.Text),2);
+end;
+
+procedure TForm1.MaskEditEx9Enter(Sender: TObject);
+begin
+DDMMSS(StrToInt(MaskEditEx7.Text),StrToInt(MaskEditEx9.Text),StrToFloat(MaskEditEx11.Text),1);
+end;
+
+procedure TForm1.BitBtn2Click(Sender: TObject);
+  var URL,k:string;
+  s,d:real;
+
+begin
+s:= StrToFloat(MaskEditEx1.Text);
+d:= StrToFloat(MaskEditEx2.Text);
+
+URL:='http://maps.yandex.ru/?ll='+FloatToStr(d)+','+FloatToStr(s)+'&z=18&l=sat,skl,&pt='+FloatToStr(d)+','+FloatToStr(s)+',pmwtm1';
+//URL:='http://maps.yandex.ru/?text='+MaskEditEx2.Text+','+MaskEditEx1.Text;
+Memo1.Text:=URL;
+ShellExecute( Handle, 'open', PChar(URL), nil, nil, SW_NORMAL );
+end;
+
+end.
---
+++ Unit2.pas
@@ -0,0 +1,174 @@
+﻿unit Unit2;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Mask, AdvSpin;
+
+type
+  TKoord = Record
+  dd:Integer;
+  mm:Integer;
+  ss:Integer;
+  end;
+  TAdvSpinEdit = class(AdvSpin.TAdvSpinEdit)
+  protected
+    procedure WndProc( var Message: TMessage ); override;
+  end;
+  TForm2 = class(TForm)
+    Label5: TLabel;
+    Label7: TLabel;
+    AdvSpinEdit3: TAdvSpinEdit;
+    AdvSpinEdit4: TAdvSpinEdit;
+    AdvSpinEdit5: TAdvSpinEdit;
+    AdvSpinEdit6: TAdvSpinEdit;
+    AdvSpinEdit7: TAdvSpinEdit;
+    AdvSpinEdit8: TAdvSpinEdit;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label3: TLabel;
+    Label6: TLabel;
+    Button1: TButton;
+    Button2: TButton;
+    AdvSpinEdit1: TAdvSpinEdit;
+    AdvSpinEdit2: TAdvSpinEdit;
+    Button3: TButton;
+    Button4: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure AdvSpinEdit1Change(Sender: TObject);
+    procedure AdvSpinEdit2Change(Sender: TObject);
+    procedure AdvSpinEdit3Change(Sender: TObject);
+    procedure AdvSpinEdit6Change(Sender: TObject);
+    procedure Shirota;
+    procedure Doldota;
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+  private
+    { Private declarations }
+
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+  uses ShellApi, Math, ClipBrd;
+
+{$R *.dfm}
+procedure TAdvSpinEdit.WndProc(var Message: TMessage);
+var c: String;
+begin
+  inherited;
+  if (Self.Name='AdvSpinEdit1') or (Self.Name='AdvSpinEdit2') then
+  begin
+  if Message.Msg = WM_PASTE then
+    begin
+    c:= Clipboard.AsText;
+    c:= StringReplace(c, '.', ',', [rfReplaceAll, rfIgnoreCase]);
+    TAdvSpinEdit(Self).Text:= c;
+    end;
+  end;
+end;
+
+procedure TForm2.Shirota;
+begin
+Edit1.Text:=AdvSpinEdit3.Text+'°'+AdvSpinEdit4.Text+'′'+AdvSpinEdit5.Text+'′′'+Button1.Caption;
+end;
+
+procedure TForm2.Doldota;
+begin
+Edit2.Text:=AdvSpinEdit6.Text+'°'+AdvSpinEdit7.Text+'′'+AdvSpinEdit8.Text+'′′'+Button2.Caption;
+end;
+
+Function DDDToDDMMSS(ddd:Real):TKoord;
+var mmm:real;
+begin
+Result.dd:=Trunc(abs(ddd));
+Result.mm:= Trunc((abs(ddd)-Result.dd)*60);
+Result.ss:=Round(((abs(ddd)-Result.dd)*60-Result.mm)*60);
+end;
+
+Function DDMMSSToDDD(dd,mm,ss:Integer):Real;
+begin
+//Result:=RoundTo(dd+(mm/60)+(ss/3600),-7);
+Result:=dd+(mm/60)+(ss/3600);
+end;
+
+procedure TForm2.AdvSpinEdit1Change(Sender: TObject);
+var koord:TKoord;
+begin
+if not AdvSpinEdit1.Focused then exit;
+
+if AdvSpinEdit1.FloatValue>0 then Button1.Caption:='N' else Button1.Caption:='S';
+koord:=DDDToDDMMSS(AdvSpinEdit1.FloatValue);
+AdvSpinEdit3.Value:=koord.dd;
+AdvSpinEdit4.Value:=koord.mm;
+AdvSpinEdit5.Value:=koord.ss;
+Shirota;
+end;
+
+procedure TForm2.AdvSpinEdit2Change(Sender: TObject);
+var koord:TKoord;
+begin
+if not AdvSpinEdit2.Focused then exit;
+if AdvSpinEdit2.FloatValue>0 then Button2.Caption:='E' else Button1.Caption:='W';
+koord:=DDDToDDMMSS(AdvSpinEdit2.FloatValue);
+AdvSpinEdit6.Value:=koord.dd;
+AdvSpinEdit7.Value:=koord.mm;
+AdvSpinEdit8.Value:=koord.ss;
+Doldota;
+end;
+
+procedure TForm2.AdvSpinEdit3Change(Sender: TObject);
+begin
+if AdvSpinEdit1.Focused then Exit;
+AdvSpinEdit1.FloatValue:=DDMMSSToDDD(AdvSpinEdit3.Value,AdvSpinEdit4.Value,AdvSpinEdit5.Value);
+Shirota;
+end;
+
+procedure TForm2.AdvSpinEdit6Change(Sender: TObject);
+begin
+if AdvSpinEdit2.Focused then Exit;
+AdvSpinEdit2.FloatValue:=DDMMSSToDDD(AdvSpinEdit6.Value,AdvSpinEdit7.Value,AdvSpinEdit8.Value);
+Doldota;
+end;
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+if Button1.Caption='N' then Button1.Caption:='S' else Button1.Caption:='N';
+AdvSpinEdit1.FloatValue:=-AdvSpinEdit1.FloatValue;
+Shirota;
+end;
+
+procedure TForm2.Button2Click(Sender: TObject);
+begin
+if Button2.Caption='E' then Button2.Caption:='W' else Button2.Caption:='E';
+AdvSpinEdit2.FloatValue:=-AdvSpinEdit2.FloatValue;
+Doldota;
+end;
+
+procedure TForm2.Button3Click(Sender: TObject);
+var url:string;
+begin
+url:='https://maps.yandex.ru/?text='+AdvSpinEdit1.Text+'+'+AdvSpinEdit2.Text;
+ShellExecute( Handle, 'open', Pchar(url), nil, nil, SW_NORMAL );
+end;
+
+procedure TForm2.Button4Click(Sender: TObject);
+var url,s,d:string;
+begin
+//url:='https://maps.google.com/?hl=ru&q=loc:'+AdvSpinEdit2.Text+'%2C'+AdvSpinEdit1.Text;
+    //https://www.google.ru/maps/place/54°29'3''N+72°52'44''E/
+s:=AdvSpinEdit3.Text+'°'+AdvSpinEdit4.Text+''''+AdvSpinEdit5.Text+''''''+Button1.Caption;
+d:=AdvSpinEdit6.Text+'°'+AdvSpinEdit7.Text+''''+AdvSpinEdit8.Text+''''''+Button2.Caption;
+url:='https://www.google.ru/maps/place/'+s+'+'+d;
+ShellExecute( Handle, 'open', Pchar(url), nil, nil, SW_NORMAL );
+end;
+
+end.
